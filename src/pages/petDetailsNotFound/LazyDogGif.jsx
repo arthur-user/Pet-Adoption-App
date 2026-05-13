c1619079589d2e9482d88c121d7db36a0c9f8463
arthur-user <115193136+arthur-user@users.noreply.github.com>
2026-05-13 14:15:35 -0700

    Delete src directory

--- src/pages/petDetailsNotFound/LazyDogGif.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import dogNotFoundGif from '../../assets/dogNotFoundGif.gif';
-
-const LazyDogGif = () => <img src={dogNotFoundGif} alt="Lost dog animation" />;
-
-export default LazyDogGif;
